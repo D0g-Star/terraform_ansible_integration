077ab32f253b4e438048b835ee07e79b52192d6b
D0g-Star <93161489+D0g-Star@users.noreply.github.com>
2022-02-23 13:15:53 -0700

    Update outputs.tf

--- terraform_test/outputs.tf
+++ terraform_test/outputs.tf
@@ -1,7 +1,14 @@
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
 }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+
+output "instance_private_dns" {
+  description = "Private_dns of the EC2 instance"
+  value       = aws_instance.app_server.private_dns
 }
