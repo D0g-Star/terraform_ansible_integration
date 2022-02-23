75ffa0368f49e9954e8ebe0cea74c5937e16d35f
D0g-Star <93161489+D0g-Star@users.noreply.github.com>
2022-02-23 13:22:06 -0700

    go back to og

--- terraform_test/outputs.tf
+++ terraform_test/outputs.tf
@@ -7,8 +7,3 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.app_server.public_ip
 }
-
-output "instance_private_dns" {
-  description = "Private_dns of the EC2 instance"
-  value       = aws_instance.app_server.private_dns
-}
