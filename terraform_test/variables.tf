621622d5ea712d7bd8ab6cba7c84bacc0d56d88e
D0g-Star <93161489+D0g-Star@users.noreply.github.com>
2022-02-23 13:29:24 -0700

    Add files via upload

--- terraform_test/variables.tf
+++ terraform_test/variables.tf
@@ -7,3 +7,8 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
 }
+
+variable "instance_name" {
+  description = "Instance Name"
+  default     = "testInstance"
+}
